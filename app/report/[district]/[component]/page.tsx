0faf65147a9fe02c635d4706ef20337dd6ab65b2
harshdubey25 <harsh.dubey@klaimify.in>
2025-10-15 14:32:36 +0530

    feat: add runtime edge in report file

--- app/report/[district]/[component]/page.tsx
+++ app/report/[district]/[component]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const runtime = 'edge';
 import { useState } from "react";
 import { useRouter } from 'next/navigation';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
