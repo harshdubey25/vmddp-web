9ca806784579409800bef8799906529994f7143e
harshdubey25 <harsh.dubey@klaimify.in>
2025-10-17 09:55:24 +0530

    fix: remove unwanted props

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -5,7 +5,7 @@ export default async function Page({ params }: { params: { lang: 'en' | 'mr' } }
   const dict = await getDictionary(params.lang);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <HeroSection dict={dict} />
+      <HeroSection/>
     </main>
   );
 }
