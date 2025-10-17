1594ff6faee0dd4f916e3aa9d852ec1d49e17f36
Samkit Jain <samkit5495@gmail.com>
2025-10-17 11:38:06 +0530

    refactor: remove unused dictionary files and related imports

--- app/[lang]/dictionaries.ts
+++
@@ -1,8 +0,0 @@
-import 'server-only';
-
-const dictionaries = {
-  en: () => import('./dictionaries/en.json').then((module) => module.default),
-  mr: () => import('./dictionaries/mr.json').then((module) => module.default),
-};
-
-export const getDictionary = async (locale: 'en' | 'mr') => dictionaries[locale]();
