89103e3a36db382132fae4fb73dcbe7d02afe649
harshdubey25 <harsh.dubey@klaimify.in>
2025-10-29 12:06:40 +0530

    feat:add new benefit points

--- app/(website)/about/page.tsx
+++ app/(website)/about/page.tsx
@@ -1,9 +1,9 @@
-import AboutSection from "@/components/AboutSection";
+import HomeAboutSection from "@/components/HomeAboutSection";
 
 export default function About() {
     return (
         <div className="min-h-[calc(100vh-16rem)]">
-            <AboutSection />
+            <HomeAboutSection />
         </div>
     );
 }
