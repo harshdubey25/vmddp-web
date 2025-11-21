232757d396db3ac9348bdeb2ae3c80ad81173060
Rahul yadu <rahul.yadu@klaimify.in>
2025-11-21 15:27:32 +0530

    feat: #VMD-140 refactor StatsCounterWrapper to use a single API call for application counts

--- components/StatsCounterWrapper.tsx
+++ components/StatsCounterWrapper.tsx
@@ -7,20 +7,15 @@ export const revalidate = 0;
 
 export default async function StatsCounterWrapper() {
     try {
-        const [approvedApplicationsCount, rejectedApplicationsCount, pendingApplicationsCount, totalApplicationsCount] = await Promise.all([
-            frappeServer.db().getCount("App Form", [["status", "=", "Approved"]]),
-            frappeServer.db().getCount("App Form", [["status", "=", "Rejected"]]),
-            frappeServer.db().getCount("App Form", [["status", "=", "Pending"]]),
-            frappeServer.db().getCount("App Form", [["status", "!=", "UnVerified"]]),
-        ]);
+        const response = await frappeServer.call().get('vmddp_app.vmddp.doctype.app_form.app_form.get_applications_by_district_component');
 
         const data = {
-            approved: approvedApplicationsCount ?? 0,
-            rejected: rejectedApplicationsCount ?? 0,
-            pending: pendingApplicationsCount ?? 0,
-            total: totalApplicationsCount ?? 0,
+            approved: response.message?.approved ?? 0,
+            rejected: response.message?.rejected ?? 0,
+            pending: response.message?.pending ?? 0,
+            total: response.message?.total ?? 0,
         };
-
+        console.log(response, data)
         return <StatsCounter data={data} />;
     } catch (error) {
         console.error("Failed to fetch application counts for AboutSection:", error);
